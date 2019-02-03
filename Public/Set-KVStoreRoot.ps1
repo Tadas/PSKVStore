9b5fbe129035ddbb6c5e07df9102d8de9c4c6c06
Tadas <thetadas+github@gmail.com>
2019-02-03 21:53:08 +0000

    Add access checks

--- Public/Set-KVStoreRoot.ps1
+++ Public/Set-KVStoreRoot.ps1
@@ -4,5 +4,20 @@
 		[string]$Path
 	)
 
+	# Test read access
+	try {
+		Get-ChildItem $Path -ErrorAction Stop
+	} catch {
+		throw "Unable to read from KVStore root: $Path"
+	}
+
+	# Test write access
+	try {
+		$TestFile = [System.IO.Path]::Combine($Path, "TestWriteAccess.txt")
+		[System.IO.File]::OpenWrite($TestFile).close()
+	} catch {
+		throw "Unable to write to KVStore root: $Path"
+	}
+
 	$script:KVStore_Root_Path = $Path
 }
